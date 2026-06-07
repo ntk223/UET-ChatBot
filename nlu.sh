a484f48ebd6f475b0b58e03ae66a49af1582e22d
Nguyen Trung Kien <23021590@vnu.edu.vn>
2026-06-07 18:42:45 +0700

    testing

--- nlu.sh
+++ nlu.sh
@@ -12,3 +12,11 @@ rasa run actions
 
 kill -9 $(lsof -ti :5006)
 python auth_server.py
+
+
+# 1. Train mô hình bằng file 80%
+rasa train nlu --nlu train_test_split/training_data.yml
+
+# 2. Đem mô hình đi thi cử bằng file test 20%
+rasa test nlu --nlu train_test_split/test_data.yml --success-on-failures
+
